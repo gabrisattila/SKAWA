39501fb9c75e13be450c6b2f0ef80fcd1615e619
Gábris Attila <gabris.attila.hun@gmail.com>
2024-02-13 15:33:53 +0100

    Created a YamlReader.java

---
+++ First_YAML_COMPILER/src/main/java/ReadFile/YamlReader.java
@@ -0,0 +1,92 @@
+package ReadFile;
+
+import java.io.*;
+import java.util.*;
+
+import static java.util.Objects.isNull;
+
+public class YamlReader {
+
+	private File fileToRead;
+
+	private final ArrayList<String> newLines;
+
+
+	public YamlReader(File file) {
+
+		if (!fileIsReadable(file))
+			throw new RuntimeException("A fájl kiterjesztése nem megfelelő, emiatt nem feldolgozható.\n (yaml vagy yml a helyes kiterjesztés)");
+
+		newLines = new ArrayList<>();
+	}
+
+
+	private boolean fileIsReadable(File file){
+		if (!file.exists())
+			return false;
+		String name = file.getName();
+		String extension = name.substring(name.lastIndexOf(".") + 1);
+		return "yaml".equals(extension) || "yml".equals(extension);
+	}
+
+
+
+	private void getNewLines() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(fileToRead));
+
+		String line = reader.readLine();
+		addNewLineToList(line);
+
+		while ((line = reader.readLine()) != null){
+			addNewLineToList(line);
+		}
+	}
+
+	private void addNewLineToList(String line){
+		String value = getValue(line);
+		newLines.add(isNull(value) ? line : changeValueIfItsReverse(line, value));
+	}
+
+	private String changeValueIfItsReverse(String line, String originValue){
+		String reversedValue = reverseValueIfItsString(originValue);
+		line = line.substring(0, line.lastIndexOf(originValue) - 1);
+		return line + reversedValue;
+	}
+
+	private String getValue(String line){
+		if ("---".equals(line))
+			return null;
+
+		String[] keyValue = parseLine(line);
+		if (!isNull(keyValue)){
+			return keyValue[1];
+		}else {
+			return null;
+		}
+	}
+
+	private String reverseValueIfItsString(String value){
+		if (!itIsProperValue(value))
+			return null;
+		return new StringBuilder(value).reverse().toString();
+	}
+
+	private boolean itIsProperValue(String value){
+		return
+				!"false".equals(value) && !"true".equals(value) &&
+				!value.matches("-?\\d+(\\.\\d+)?");
+	}
+
+
+	private String[] parseLine(String line){
+		String[] keyValue = line.split(": ");
+		if (keyValue.length == 1){
+			if (keyValue[0].contains("\t"))
+				keyValue = line.split("- ");
+			else
+				return null;
+		}
+		return keyValue;
+	}
+
+}
---
+++ First_YAML_COMPILER/src/main/java/ReadFile/YamlWriter.java
@@ -0,0 +1,14 @@
+package ReadFile;
+
+import java.util.ArrayList;
+
+public class YamlWriter {
+
+	private final ArrayList<String> linesToWrite;
+
+	public YamlWriter(ArrayList<String> linesToWrite) {
+		this.linesToWrite = linesToWrite;
+	}
+
+
+}
